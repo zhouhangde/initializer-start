f2e475e1ba0ad074bbe0baad988aa1d4b1daf5cc
zhouhangde <1728451471@qq.com>
2019-03-29 13:19:35 +0800

    zhou

---
+++ src/main/java/com/waylau/spring/boot/blog/initializerstart/InitializerStartApplication.java
@@ -0,0 +1,13 @@
+package com.waylau.spring.boot.blog.initializerstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializerStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializerStartApplication.class, args);
+	}
+
+}
